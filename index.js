465afbac27c13d5a0f6e6674d4bb4cfdcec71b7d
trev97 <trevorscogin97@gmail.com>
2023-02-23 14:27:40 -0600

    Added ability to add query parameters to the URL

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var app = express();
+const querystring = require('querystring')
 const http = require('https')
-const init = {
+let init = {
    host : 'external-api.faa.gov',
    path: "/notamapi/v1/notams",
    method: "GET",
    headers: {
       'client_id': '4e25e8c041d142d9b263f03aa74be97e',
       'client_secret': 'B7282F38DC454fBFBFF80774Ec4D1772'
-   }
+   } 
 }
 
+let searchParams = {
+  domesticLocation: 'BHM',
+  sortBy: 'notamType'
+}
+
+const res_query = querystring.stringify(searchParams)
+init.path += '?'+ res_query
+console.log(init)
 const callback = function(response) {
   
   let result = Buffer.alloc(0);
@@ -21,7 +30,7 @@ const callback = function(response) {
   response.on('end', function() {
     // result has response body buffer
     s = JSON.parse(result.toString());
-    console.log(result.toString());
+    //console.log(result.toString());
     app.get('/', function(req, res){
       res.send(s);
    });
